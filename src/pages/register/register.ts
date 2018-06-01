c34e9588054e982dbab33d82d2d956a780d6fbce
bilal <bilal.hammoud@appinio.de>
2018-06-01 15:06:35 +0200

    Verify Email (finished)

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,9 +1,10 @@
 import {Component} from '@angular/core';
-import {IonicPage, LoadingController, NavController, NavParams, ToastController} from 'ionic-angular';
+import {AlertController, IonicPage, LoadingController, NavController, NavParams, ToastController} from 'ionic-angular';
 import {User} from "../../models/user";
 
 import {AngularFireAuth} from 'angularfire2/auth';
 import {TabsPage} from "../tabs/tabs";
+import {HomePage} from "../home/home";
 
 @IonicPage()
 @Component({
@@ -13,15 +14,55 @@ import {TabsPage} from "../tabs/tabs";
 export class RegisterPage {
 
   repeatPassword: string;
+  DSGVOAgreed: boolean = false;
 
   user = {} as User;
 
-  constructor(private afAuth: AngularFireAuth, public navCtrl: NavController, public navParams: NavParams,
+  constructor(private afAuth: AngularFireAuth,
+              public navCtrl: NavController,
+              public navParams: NavParams,
               public loadingCtrl: LoadingController,
+              public alertCtrl: AlertController,
               public toastCtrl: ToastController) {
   }
 
   ionViewDidLoad() {
+    if (false === this.DSGVOAgreed) {
+      this.alertCtrl.create({
+        title: 'EU-DSGVO',
+        message: 'Do you agree?',
+        enableBackdropDismiss: false,
+        buttons: [
+          {
+            text: 'Disagree',
+            role: 'cancel',
+            handler: () => {
+              this.navCtrl.setRoot(HomePage);
+            }
+          },
+          {
+            text: 'Agree',
+            role: 'confirm',
+            handler: () => {
+              this.DSGVOAgreed = true;
+            }
+          }
+        ]
+      }).present();
+    }
+  }
+
+  sendEmailVerification(){
+    this.afAuth.authState.subscribe(user => {
+      user.sendEmailVerification()
+        .then(() => {
+          this.toastCtrl.create({
+            message: 'Email verification sent.',
+            duration: 3000,
+            position: 'top'
+          }).present();
+        })
+    });
   }
 
   async register(user: User) {
@@ -48,6 +89,8 @@ export class RegisterPage {
 
       try {
         const result = await this.afAuth.auth.createUserWithEmailAndPassword(user.email, user.password).then(authData => {
+          this.sendEmailVerification();
+
           //successful
           loader.dismiss();
           this.navCtrl.setRoot(TabsPage);
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,14 +3,17 @@ import {Component} from '@angular/core';
 import {AboutPage} from '../about/about';
 import {ContactPage} from '../contact/contact';
 import {StartPage} from '../startPage/startPage';
-import {AlertController, ToastController} from "ionic-angular";
+import {AlertController, NavController, ToastController} from "ionic-angular";
 
 import {AngularFireAuth} from 'angularfire2/auth'
+import {HomePage} from "../home/home";
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
+
+  user: any;
 
   tab1Root = StartPage;
   tab2Root = AboutPage;
@@ -18,6 +21,7 @@ export class TabsPage {
 
   constructor(private afAuth: AngularFireAuth,
               public toast: ToastController,
+              public navCtrl: NavController,
               private alertCtrl: AlertController) {
   }
 
@@ -36,5 +40,35 @@ export class TabsPage {
         }).present();
       }
     });
+    this.checkVerifiedEmail();
+  }
+
+  checkVerifiedEmail (){
+    this.user = this.afAuth.auth.currentUser;
+
+    if (this.user && !this.user.emailVerified) {
+      this.alertCtrl.create({
+        title: 'Verify Email',
+        message: 'Please check the mailbox to verify your Email',
+        enableBackdropDismiss: false,
+        buttons: [
+          {
+            text: 'I verified it',
+            role: 'refresh',
+            handler: () => {
+              this.user.reload();
+              this.user.getIdToken(true);
+              this.checkVerifiedEmail();
+            }
+          },
+          {
+            text: 'Contact us',
+            role: 'contact',
+            handler: () => {
+            }
+          }
+        ]
+      }).present();
+    }
   }
 }
